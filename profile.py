19615133b777de6d50102a626dbf5a3ae9dc366b
Veeravel <aveera94@gmail.com>
2020-01-18 19:32:25 +0530

    Scrape Instagram using Python

---
+++ profile.py
@@ -0,0 +1,52 @@
+import requests
+import json
+import re
+from bs4 import BeautifulSoup
+
+class Profile(object):
+
+    
+    def __init__(self, username='instagram'):
+        """
+        Get basic user profile details from instagram page without login.
+        Required:
+            None
+        Optional:
+            1. username - Username of the specific user - by default it will provide you the Instagram profile details
+        Returns:
+            None
+        """
+        self.username = username
+        self.url = "https://www.instagram.com/" + username
+        self.session = requests.Session()
+    
+    def get_profile_data(self):
+        """
+        Get profile data for the user
+        """
+        response = self.session.get(self.url)
+        if response.status_code != 200:
+            raise Exception("Invalid request or data not found: {}".format(response.status_code))
+        response_data = self._parse_instagram_response(response_data=response.text)
+        return response_data
+    
+    def _parse_instagram_response(self, *, response_data):
+        """
+        Parsing instagram response
+        Parameters:
+            1. Required:
+                response_data - Instgram request response data
+            2. Optional:
+                None
+            3. Returns:
+                Parsed data of instagram response
+        """
+        pattern = r'window\._sharedData\s=\s(.*);$'
+        soup = BeautifulSoup(response_data, 'html.parser')
+        find_data = soup.find('script',type='text/javascript', text=re.compile(pattern)).text
+        match = re.match(pattern,find_data)
+        json_data = json.loads(match.group(1))
+        profile_page = json_data['entry_data']['ProfilePage'][0]
+        user_details = profile_page['graphql']['user']
+        return user_details
+
