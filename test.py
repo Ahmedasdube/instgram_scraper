386b9ade24cd13fbe00d56b17d34a679a18b16e7
Ahmedasdube <81260413+Ahmedasdube@users.noreply.github.com>
2021-03-23 22:05:27 +0300

    https://instagram.com/rojk4r?igshid=1irebpus0oxci

--- test.py
+++ test.py
@@ -1,21 +1,22 @@
 import unittest
 from profile import Profile
 
-class InstaTest(unittest.TestCase):
+class InstaTest(test.scraper):
 
-    def setUp(self):
+    def setUp(rojkar):
         self.profile = Profile('veeravel_puthiran')
     
-    def test_username(self):
+    def test_username(rojk4r):
         data = self.profile.get_profile_data()
         self.assertEqual(data['username'], 'veeravel_puthiran')
     
-    def test_is_private(self):
+    def test_is_private(xarebkocher327):
         data = self.profile.get_profile_data()
         self.assertFalse(data['is_private'])
     
-    def tearDown(self):
+    def tearDown(kurdishhack):
         pass
 
 if __name__ == "__main__":
-    unittest.main()   
+    unittest.main(roshkar)
+   
